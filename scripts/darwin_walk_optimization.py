a89ae647c27d1f19f0bc0c708b36aefe07f1147b
sayantan <4auddy@informatik-uni-hamburg.de>
2016-07-07 17:40:11 +0200

    First draft of walk optimization code

--- scripts/darwin_walk_optimization.py
+++ scripts/darwin_walk_optimization.py
@@ -6,12 +6,24 @@
 # Execution: rosrun robot_walking darwin_walk_optimization.py
 ###############################################################################
 
+import rospy
 import numpy as np
 import matplotlib.pyplot as plt
 import math
 import random
 import time
-
+# Import messages
+from gazebo_msgs.msg import ModelStates
+from gazebo_msgs.msg import ModelState
+from sensor_msgs.msg import JointState
+from sensor_msgs.msg import Imu
+from geometry_msgs.msg import Pose, Twist
+# Import services
+from gazebo_msgs.srv import GetModelState
+from gazebo_msgs.srv import SetModelState
+from controller_manager_msgs.srv import ListControllers
+# Import models
+from darwin_gazebo.darwin import Darwin
 
 class Darwin_Walk_Optimization():
 
@@ -36,12 +48,12 @@ class Darwin_Walk_Optimization():
         
         # PSO specific initialization
         
-        self.POPULATION_SIZE = 100
+        self.POPULATION_SIZE = 10
         # Each individual consists of 24 positions (weights, and oscillator constants), 
         # 24 velocities, 24 PBest positions and 1 PBest score
         self.INDIVIDUAL_VECTOR_SIZE = 73
         self.GBEST_VECTOR_SIZE = 25
-        self.MAX_ITERS = 200
+        self.MAX_ITERS = 20
         self.C1 = 2.0 # Acceleration constant
         self.C2 = 2.0 # Acceleration constant
         # Inertial weight, linearly scales from 0.9 to 0.4 over the entire run
@@ -77,39 +89,92 @@ class Darwin_Walk_Optimization():
         self.joint_lower_limits['j_tibia_r']  = 0.0
         self.joint_lower_limits['j_ankle1_r'] = -1.047        
 
-        # To implement:
-        # Mechanism to stop trial if robot falls
-        # Reset joint positions
-        # Return distance travelled as a fitness value
+        # Variable to track distance walked by each individual
+        self.distance_walked = 0.0
         
-        ########################################################################################## 
+        # Variables to track the position of the robot
+        self.current_model_x = 0.0
+        self.current_model_y = 0.0
+        self.current_model_z = 0.0
         
+        # Variable (list) to track the height of the robot
+        self.model_polled_z = [] 
+        
+        # Variable to tracj=k if the robot has fallen
+        self.has_fallen = False
+        
+        # Constant fall limit - If the z coordinate of the robot is below this, then the robot has fallen
+        self.DARWIN_COM_FALL_LIMIT = 0.2 
+
+
+        # ROS specific initialization
+        
+        self.model_name = 'darwin'
+        self.relative_entity_name = 'world'
+        
+        # Initialize the ROS node
+        rospy.init_node('darwin_walk_demo', anonymous=False)
+
+        # Subscribe to the /gazebo/model_states topic
+        rospy.Subscriber("/gazebo/model_states", ModelStates, self.subscriber_callback_modelstate)
+        
+        # Register the client for service gazebo/get_model_state
+        rospy.wait_for_service('/gazebo/get_model_state')
+        self.get_model_state = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
+
+        # Register the client for service gazebo/set_model_state
+        rospy.wait_for_service('/gazebo/set_model_state')
+        self.set_model_state = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
+        
+        # Register the client for service /darwin/controller_manager/list_controllers
+        rospy.wait_for_service('/darwin/controller_manager/list_controllers')
+        self.get_joint_state = rospy.ServiceProxy('/darwin/controller_manager/list_controllers', ListControllers)
+        
+        # Create the Darwin model
+        self.darwin = Darwin()
+
+
+        # Initialize log file
+        
+        # Current time
+        curr_time = time.strftime('%Y%m%d%H%M%S')
+        
+        # Create a log file
+        self.log_file = open('/home/sayantan/Knowledge/Independant Studies/Robot Walking/code/robot_walking/data/log_'+str(curr_time)+'_darwin_walk_optimization.log', 'w')
+        self.log_file.write('Created log file at ' + str(curr_time) + '\n')
+                      
+
+        # Initiate the walk optimization        
         self.pso()
+
+
 
     # Function to calculate the next state of the Matsuoka neural oscillators
     def matsuoka(self, individual, state):
+    
+        print state
 
         # Extract the weights
-        w0  = individual[0]        
-        w1  = individual[1]
-        w2  = individual[2]
-        w3  = individual[3]
-        w4  = individual[4]
-        w5  = individual[5]
-        w6  = individual[6]
-        w7  = individual[7]
-        w8  = individual[8]
-        w9  = individual[9]
-        w10 = individual[10]
-        w11 = individual[11]
-        w12 = individual[12]
-        w13 = individual[13]
-        w14 = individual[14]
-        w15 = individual[15]
-        w16 = individual[16]
-        w17 = individual[17]
-        w18 = individual[18]   
-        w19 = individual[19]      
+        w1  = individual[0]        
+        w2  = individual[1]
+        w3  = individual[2]
+        w4  = individual[3]
+        w5  = individual[4]
+        w6  = individual[5]
+        w7  = individual[6]
+        w8  = individual[7]
+        w9  = individual[8]
+        w10  = individual[9]
+        w11 = individual[10]
+        w12 = individual[11]
+        w13 = individual[12]
+        w14 = individual[13]
+        w15 = individual[14]
+        w16 = individual[15]
+        w17 = individual[16]
+        w18 = individual[17]
+        w19 = individual[18]   
+        w20 = individual[19]      
         
         # Extract the network constants
         Tr = individual[20]
@@ -121,49 +186,48 @@ class Darwin_Walk_Optimization():
         # The numbering scheme: For f21 <variable=f><oscillator#=2><1 for extensor, 2 for flexor>
         # Each oscillator has the state [x1,x2,f1,f2,y1,y2]
         # There are 6 such oscillators
-        x11 = state[0][0] 
-        x12 = state[0][1] 
-        f11 = state[0][2] 
-        f12 = state[0][3] 
-        y11 = state[0][4]
-        y12 = state[0][5]
+        x11 = state[0] 
+        x12 = state[1] 
+        f11 = state[2] 
+        f12 = state[3] 
+        y11 = state[4]
+        y12 = state[5]
 
-        x21 = state[1][0] 
-        x22 = state[1][1] 
-        f21 = state[1][2] 
-        f22 = state[1][3] 
-        y21 = state[1][4]
-        y22 = state[1][5]
+        x21 = state[6] 
+        x22 = state[7] 
+        f21 = state[8] 
+        f22 = state[9] 
+        y21 = state[10]
+        y22 = state[11]
     
-        x31 = state[2][0] 
-        x32 = state[2][1] 
-        f31 = state[2][2] 
-        f32 = state[2][3] 
-        y31 = state[2][4]
-        y32 = state[2][5]
+        x31 = state[12] 
+        x32 = state[13] 
+        f31 = state[14] 
+        f32 = state[15] 
+        y31 = state[16]
+        y32 = state[17]
 
-        x41 = state[3][0] 
-        x42 = state[3][1] 
-        f41 = state[3][2] 
-        f42 = state[3][3] 
-        y41 = state[3][4]
-        y42 = state[3][5]
+        x41 = state[18] 
+        x42 = state[19] 
+        f41 = state[20] 
+        f42 = state[21] 
+        y41 = state[22]
+        y42 = state[23]
 
-        x51 = state[4][0] 
-        x52 = state[4][1] 
-        f51 = state[4][2] 
-        f52 = state[4][3] 
-        y51 = state[4][4]
-        y52 = state[4][5]
+        x51 = state[24] 
+        x52 = state[25] 
+        f51 = state[26] 
+        f52 = state[27] 
+        y51 = state[28]
+        y52 = state[29]
 
-        x61 = state[5][0] 
-        x62 = state[5][1] 
-        f61 = state[5][2] 
-        f62 = state[5][3] 
-        y61 = state[5][4]
-        y62 = state[5][5]
-        
-        
+        x61 = state[30] 
+        x62 = state[31] 
+        f61 = state[32] 
+        f62 = state[33] 
+        y61 = state[34]
+        y62 = state[35]
+                
         # Calculate the derivatives of the membrane potentials
         # This is based on the connections between the individual neurons
         x11_d = ((-1.0*w1*y12) - (w6*y21) + (s) - (b*f11) - (x11))/float(Tr)
@@ -251,7 +315,12 @@ class Darwin_Walk_Optimization():
 
     # Function for Walking using Matsuoka Oscillator
     def matsuoka_walking_fitness(self,individual,TRIAL_DURATION):
-    
+
+        print 'Individual:' + str(individual)
+
+        # Reset the simulation before each walk
+        self.reset_simulation()
+                
         # Set initial state - [x1,x2,f1,f2,y1,y2] for each oscillator
         # There are 6 oscillators, one each for the joints j_thigh2_l,j_tibia_l,j_ankle1_l,j_thigh2_r,j_tibia_r,j_ankle1_r
 
@@ -313,42 +382,108 @@ class Darwin_Walk_Optimization():
             # The last parameter is the delay, this should match the step size
             self.darwin.set_angles_slow(new_angles,self.STEP_DURATION)
             
+            # Calculate the current distance travelled by the robot
+            self.distance_walked = np.sign(self.current_model_x)*math.sqrt((self.current_model_x)*(self.current_model_x) + (self.current_model_y)*(self.current_model_y))
+            
+            # If the robot has fallen, return the distance travelled till now as the fitness score
+            if(self.has_fallen): return self.distance_walked
+            
             i=i+1
+            
+        # If the trial duration has ended return the distance travelled as the fitness score
+        return self.distance_walked
         
 
     def reset_simulation(self):
-        # Empty the self.model_polled_z list
-        self.model_polled_z = []
-        
+                
+        # Reset the joints
+        new_angles = {}
+        new_angles['j_ankle1_l']   = 0.0
+        new_angles['j_ankle1_r']   = 0.0 
+        new_angles['j_ankle2_l']   = 0.0 
+        new_angles['j_ankle2_r']   = 0.0 
+        new_angles['j_gripper_l']  = 0.0 
+        new_angles['j_gripper_r']  = 0.0 
+        new_angles['j_high_arm_l'] = 1.50 # Arm by the side of the body
+        new_angles['j_high_arm_r'] = 1.50 # Arm by the side of the body
+        new_angles['j_low_arm_l']  = 0.0 
+        new_angles['j_low_arm_r']  = 0.0 
+        new_angles['j_pan']        = 0.0
+        new_angles['j_pelvis_l']   = 0.0 
+        new_angles['j_pelvis_r']   = 0.0 
+        new_angles['j_shoulder_l'] = 0.0 
+        new_angles['j_shoulder_r'] = 0.0 
+        new_angles['j_thigh1_l']   = 0.0
+        new_angles['j_thigh1_r']   = 0.0 
+        new_angles['j_thigh2_l']   = 0.0 
+        new_angles['j_thigh2_r']   = 0.0 
+        new_angles['j_tibia_l']    = 0.0 
+        new_angles['j_tibia_r']    = 0.0 
+        new_angles['j_tilt']       = 0.0
+        new_angles['j_wrist_l']    = 0.0 
+        new_angles['j_wrist_r']    = 0.0
+        # Default delay of setting angles is 2 seconds
+        self.darwin.set_angles_slow(new_angles)
+
+
+        # Reset the robot position        
         # Send model to x=0,y=0
-        model_name = 'darwin'
         pose = Pose()
         pose.position.z = 0.31
         twist = Twist()
-        reference_frame = 'world'
         
         md_state = ModelState()
-        md_state.model_name = model_name
+        md_state.model_name = self.model_name
         md_state.pose = pose
         md_state.twist = twist
-        md_state.reference_frame = reference_frame
+        md_state.reference_frame = self.relative_entity_name
 
         # Service call to reset model
         try:
             response = self.set_model_state(md_state)             
         except rospy.ServiceException, e:
-            print "Service call failed: %s"%e
+            print "Darwin model state initialization service call failed: %s"%e
             
         rospy.loginfo("Result of model reset: " + str(response))
-        
-        # Reset the joints - Not implemented for now
+
+
+        # Reset the distance variable
+        self.distance_walked = 0.0
+
+        # Reset the position variables
+        self.current_model_x = 0.0
+        self.current_model_y = 0.0
+        self.current_model_z = 0.0
+
+        # Reset the height list
+        self.model_polled_z = []
+
+
+    # Function to retrieve the current model state - the position and orientation of the robot
+    def subscriber_callback_modelstate(self, dummymodelstate):
+        try:
+            modelstate = self.get_model_state(self.model_name,self.relative_entity_name)
+            
+            # Retrieve model x,y and z coordinates
+            self.current_model_x = modelstate.pose.position.x
+            self.current_model_y = modelstate.pose.position.y
+            self.current_model_z = modelstate.pose.position.z
+            
+            # Store the current z coordinate in the list
+            self.model_polled_z.append(self.current_model_z)
+            
+            # Check if the robot has fallen
+            if(self.current_model_z < self.DARWIN_COM_FALL_LIMIT):
+                self.has_fallen = True
+              
+        except rospy.ServiceException, e:
+            print "Service call failed: %s"%e
 
 
 
     # Function for Particle Swarm Optimization
     def pso(self):
         
-
         # PSO specific variables
         
         # The GBest vector - The first 24 elements are positions, the last element is 
@@ -419,9 +554,10 @@ class Darwin_Walk_Optimization():
             index += 1
 
 
-                    
+        # Main PSO logic     
         iteration = 0
         while(iteration < self.MAX_ITERS):
+            
             
             # Set the linearly scaled inertial weight
             w = self.INERTIAL_WEIGHT_BOUNDS[1] - (iteration*(self.INERTIAL_WEIGHT_BOUNDS[1] - self.INERTIAL_WEIGHT_BOUNDS[0])/self.MAX_ITERS)
@@ -549,8 +685,9 @@ class Darwin_Walk_Optimization():
                 
             iteration += 1
             
-            # Print the results
-            print "Best fitness: " + str(self.GBest[24]) + " at " + str(self.GBest[0:24])
+            # Log the results
+            curr_time = time.strftime('%Y%m%d%H%M%S')
+            self.log_file.write('[' + str(curr_time) + ']' + ' GBest Fitness: ' + str(self.GBest[24]) + ' GBest position: ' + str(self.GBest[0:24]) +'\n')
 
 if __name__ == '__main__':
 
