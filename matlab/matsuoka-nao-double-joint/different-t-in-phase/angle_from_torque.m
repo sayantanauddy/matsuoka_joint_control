b29e8c43bfaa2c06302818810e7da2e6988399d7
sayantan <4auddy@informatik-uni-hamburg.de>
2016-11-17 19:40:56 +0100

    Added scripts and video for in-phase double joint oscillations with different frequencies

---
+++ matlab/matsuoka-nao-double-joint/different-t-in-phase/angle_from_torque.m
@@ -0,0 +1,117 @@
+% Script to compute the behavior of a single joint driven by a set of
+% neural Matsuoka oscillators
+
+clearvars;
+clearvars -global;
+
+% Oscillator constants for oscillator 1
+global t1_l;
+global t2_l;
+global t1_r;
+global t2_r;
+global beta;
+global eta;
+global sigma;
+global mu;
+global nu;
+global h_psi;
+T_l = 2.4;
+t1_l = 2.13 + 0.6804*T_l - sqrt(4.512 + 2.685*T_l);
+t2_l = 2.5*t1_l;
+T_r = 1.2;
+t1_r = 2.13 + 0.6804*T_r - sqrt(4.512 + 2.685*T_r);
+t2_r = 2.5*t1_r;
+beta = 2.5;
+eta = 2.5;
+sigma = 1.5;
+mu = 0.75;
+nu = 0.49;
+h_psi = 5.0;
+
+% State variables of oscillator 1
+global psi_l_i;
+global psi_l_j;
+global phi_l_i;
+global phi_l_j;
+global u_l_i;
+global u_l_j;
+psi_l_i = 0.0;
+psi_l_j = 1.0;
+phi_l_i = 0.0;
+phi_l_j = 1.0;
+u_l_i = 1;
+u_l_j = 1;
+
+% State variables of oscillator 2
+global psi_r_i;
+global psi_r_j;
+global phi_r_i;
+global phi_r_j;
+global u_r_i;
+global u_r_j;
+psi_r_i = 0.0;
+psi_r_j = 1.0;
+phi_r_i = 0.0;
+phi_r_j = 1.0;
+u_r_i = 1;
+u_r_j = 1;
+
+% Mean position of oscillation of the joint angle
+global theta_star;
+theta_star = 0.0;
+
+% Desired time period of joint oscillations
+global T_joint;
+T_joint= 2;
+
+% Variables required to calculate the time delta
+global time_prev;
+global time_now;
+time_prev = 0;
+time_now = 0;
+
+% List used for plotting the torques
+global torque_list_l;
+torque_list_l = [];
+global torque_list_r;
+torque_list_r = [];
+
+% List for average position
+global avg_position_l;
+avg_position_l = [];
+global avg_position_r;
+avg_position_r = [];
+
+% List for ut
+global ut_list_l;
+ut_list_l = [];
+global ut_list_r;
+ut_list_r = [];
+
+% Set the time steps
+t = [0:0.01:30];
+
+% Solve the differential equations of the inertial mechanical system
+% Use custom ODE solver which uses fixed time steps
+% Matlab's in-built ODE solver uses variable time steps and sometimes goes
+% back in time to adjust the time step. This causes problems in the state
+% variables of the matsuoka oscillator.
+y=ode1(@inertial_ode,t,[0.0; 0.0; 0.0; 0.0]);
+
+% Plot the results
+
+hold on;
+plot(1);
+p1 = plot(t,y(:,1),'b');
+p2 = plot(t,y(:,3),'r');
+p1(1).LineWidth = 2;
+p2(1).LineWidth = 2;
+xlabel('time (s)');
+ylabel('joint angle (rad)');
+set(gca,'fontsize',20);
+legend([p1,p2],'left joint angle', 'right joint angle');
+%grid on;
+%grid minor;
+
+
+
---
+++ matlab/matsuoka-nao-double-joint/different-t-in-phase/readme.txt
@@ -0,0 +1,12 @@
+Here the right arm has a set of t1,t2 and the left arm has a set of different t1 and t2. (T_r=1.2 and T_l=2.4)
+
+Coupling factor of right arm in left torque = 0.5*mu, 0.5*nu
+u_i,u_j for left arm is also 0.5 of that of the right arm (see matsuoka_torque.m)
+
+The right arm is switched on 1.6 seconds after the start.
+
+The coupling is switched on after 10s from the start.
+
+Once the coupling takes effect, we can see that the right arm is almost in-phase with the left but has nearly double the frequency.
+
+
